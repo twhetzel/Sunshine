722fe4a53b87e637f1deefdb8b21072d4b43c70a
Trish Whetzel <plwhetzel@gmail.com>
2015-02-07 23:51:08 -0800

    add implicit intent to open map based on postal location

--- app/src/main/java/com/example/whetzel/sunshine/MyActivity.java
+++ app/src/main/java/com/example/whetzel/sunshine/MyActivity.java
@@ -1,13 +1,18 @@
 package com.example.whetzel.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MyActivity extends ActionBarActivity {
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +26,7 @@ public class MyActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) { //TODO: Why is MenuInflater not also passed in like in ForecastFragment
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
         return true;
@@ -37,10 +42,34 @@ public class MyActivity extends ActionBarActivity {
             // Add Explicit Intent to launch SettingsActivity
             Intent launchSettingsActivity = new Intent(this, SettingsActivity.class);
             startActivity(launchSettingsActivity);
-
+            return true;
+        }
+        if (id == R.id.action_map) {
+            openPreferredLocationInMap();
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap() {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+
+        // Get Location
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+        Log.d(TAG, "Location: "+geoLocation);
+
+        // See docs for CommonIntents https://developer.android.com/guide/components/intents-common.html#Maps
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(TAG, "Couldn't call " + location + ", no receiving apps installed!");
+        }
+
     }
 
 }
